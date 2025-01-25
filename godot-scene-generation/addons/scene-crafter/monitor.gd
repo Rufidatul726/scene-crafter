307b6f84265ff5c5da6ed999f149deba5599c260
Rufidatul726 <bsse1226@iit.du.ac.bd>
2025-01-26 01:25:49 +0600

    updating plugin

--- godot-scene-generation/addons/scene-crafter/monitor.gd
+++ godot-scene-generation/addons/scene-crafter/monitor.gd
@@ -1,8 +1,6 @@
 @tool
 extends EditorPlugin
 
-var log_file
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var http := HTTPRequest.new()
@@ -10,12 +8,14 @@ func _ready() -> void:
 	http.connect("request_completed", Callable(self, "_on_request_completed"))
 	
 	http.request(
-		"http://127.0.0.1:8000/getdir/",  # Your Python server endpoint
-		["Content-Type: application/json"], # Headers
+		"http://127.0.0.1:8000/getdir/",
+		[], # Headers
 		HTTPClient.METHOD_GET
 	)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_request_completed(result, response_code, headers, body) -> void:
+	var response = JSON.parse_string(body.get_string_from_utf8())
+	if response_code == 200:
+		print("Response from Python:", response)
+	else:
+		print("Error:", response_code, response)
--- godot-scene-generation/addons/scene-crafter/scene-crafter.gd
+++ godot-scene-generation/addons/scene-crafter/scene-crafter.gd
@@ -4,11 +4,22 @@ extends EditorPlugin
 var dock
 var scene_folder := "res://addons/scene-crafter/generated-scene"
 
+@export var api_url : String = "https://api.groq.com/openai/v1/chat/completions"
+var api_key : String 
+
+var http_request : HTTPRequest
+var prompt_input : TextEdit
+var response_label : Label
+
 func _enter_tree() -> void:
 	dock = preload("res://addons/scene-crafter/generate_scene.tscn").instantiate()
 	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_UL, dock)
-
+	
+	start_monitoring()
 
 func _exit_tree() -> void:
 	remove_control_from_docks(dock)
 	dock.free()
+
+def start_monitoring():
+	
